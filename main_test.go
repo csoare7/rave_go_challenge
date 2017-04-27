4357e0675be16f867601ffa8e8c774c946f17351
Claudiu S <csoare7@gmail.com>
2017-04-28 00:54:07 +0100

    Adds asserts for content type on GetIndex

--- main_test.go
+++ main_test.go
@@ -4,15 +4,15 @@ import (
   "net/http"
   "net/http/httptest"
   "testing"
+  "strings"
 )
 
 func TestGetIndex(t *testing.T) {
-  // Create a request to pass to our handler.
   req, err := http.NewRequest("GET", "/", nil)
   if err != nil {
     t.Fatal(err)
   }
-  
+
   rr := httptest.NewRecorder()
   handler := http.HandlerFunc(GetIndex)
 
@@ -21,10 +21,21 @@ func TestGetIndex(t *testing.T) {
   if status := rr.Code; status != http.StatusOK {
     t.Errorf("handler returned wrong status code: got %v want %v", status, http.StatusOK)
   }
+
+  var contentTypeExpected string = "text/html; charset=UTF-8"
+
+  if contentType, present := rr.HeaderMap["Content-Type"]; present {
+    // convert []string to string 
+    contentType := strings.Join(contentType, "")
+    if contentType != contentTypeExpected {
+      t.Errorf("handler returned wrong content type: got %v want %v", contentType, contentTypeExpected)
+    }
+  } else {
+    t.Errorf("handler returned no Content-Type set")
+  }
 }
 
 func TestPostIndex(t *testing.T) {
-  // Create a request to pass to our handler.
   req, err := http.NewRequest("POST", "/", nil)
   if err != nil {
     t.Fatal(err)
